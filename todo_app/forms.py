1210ef4f18b285d78b870eda70f11dd38a5686be
brikoz <brikocorp@gmail.com>
2024-01-25 19:29:39 +0300

    Finally commit todo APP

--- todo_app/forms.py
+++ todo_app/forms.py
@@ -1,14 +1,27 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, DateField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, ValidationError, EqualTo
 
 from .forms_custom_validators import DateCheck
+from .models import User
 
 
-class UserForm(FlaskForm):
+class UserLoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=32)])
     submit = SubmitField('Submit')
+
+
+class UserRegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=32)])
+    password_repeat = PasswordField('Repeat password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Submit')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("This username already exists!")
 
 
 class TaskForm(FlaskForm):
--- todo_app/routes.py
+++ todo_app/routes.py
@@ -1,8 +1,10 @@
+import datetime
+
 from flask import render_template, redirect, url_for, flash
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 
 from todo_app import app, login, db
-from .forms import UserForm, TaskForm
+from .forms import UserLoginForm,UserRegistrationForm, TaskForm
 from .models import User, Task
 
 
@@ -11,23 +13,29 @@ def mainpage():  # put application's code here
     form = TaskForm()
     if current_user.is_authenticated:
         tasks = Task.query.filter_by(user_id=current_user.id)
+        for task in tasks:
+            if task.expire_date.isocalendar() < datetime.date.today().isocalendar() and task.is_expired == False:
+                flash(f'Task {task.task} expired!')
+                task.is_expired = True
+                db.session.commit()
     else:
         tasks = None
     if form.validate_on_submit():
         task = Task(task=form.task.data, expire_date=form.expire_date.data, user_id=current_user.id)
         db.session.add(task)
         db.session.commit()
-        flash('Task succesfully added!')
+        flash('Task succesfully added!', 'notice')
         return redirect(url_for('mainpage'))
     return render_template('index.html', form=form, tasks=tasks)
 
 
 @app.route('/<int:task_id>')
+@login_required
 def delete_task(task_id):
     task = Task.query.get(task_id)
     db.session.delete(task)
     db.session.commit()
-    flash('Task succesfully deleted!')
+    flash('Task succesfully deleted!', 'notice')
     return redirect(url_for('mainpage'))
 
 
@@ -40,11 +48,11 @@ def load_user(id):
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('mainpage'))
-    form = UserForm()
+    form = UserLoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
+            flash('Invalid username or password', "error")
             return redirect(url_for('login'))
         login_user(user)
         return redirect(url_for('mainpage'))
@@ -55,13 +63,13 @@ def login():
 def signup():
     if current_user.is_authenticated:
         return redirect(url_for('mainpage'))
-    form = UserForm()
+    form = UserRegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data)
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
-        flash('You are registered now!')
+        flash('You are registered now!', 'notice')
         return redirect(url_for('mainpage'))
     return render_template('signup.html', form=form)
 
